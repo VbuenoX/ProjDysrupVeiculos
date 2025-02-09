da54f091217b0783cda2a21bb55ee42d6b5dbc01
Vitor Bueno <143560403+VbuenoX@users.noreply.github.com>
2025-02-09 18:56:21 -0300

    Config Login Google

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,13 @@
+import { TouchableOpacityProps } from "react-native";
 import { Container, Title } from "./styles";
 
-interface ButtonProps {
+interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
-export function Button({ title }: ButtonProps) {
+export function Button({ title, onPress }: ButtonProps) {
   return (
-    <Container activeOpacity={0.7}>
+    <Container activeOpacity={0.7} onPress={onPress}>
       <Title>{title}</Title>
     </Container>
   );
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,32 @@
 import { Container, Title, Slogan } from "./styles";
 import { Button } from "../../components/Button";
 import backgroundImg from "../../assets/background.png";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { WEB_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { useState } from "react";
+import { Alert } from "react-native";
+
+GoogleSignin.configure({
+  scopes: ["email", "profile"],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
 
 export function SignIn() {
+  async function handleGoogleSignIn() {
+    try {
+      const response = await GoogleSignin.signIn();
+    } catch (error) {
+      console.error(error);
+      Alert.alert("Não foi possível fazer login com o Google");
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Dysrup Veiculos</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
-      <Button title="Entrar com o Google"></Button>
+      <Button title="Entrar com o Google" onPress={handleGoogleSignIn} />
     </Container>
   );
 }
